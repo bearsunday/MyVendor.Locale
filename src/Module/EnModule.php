27393fa0e7b19a93b7c85dff9bc4b5336ac0f0fe
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-11 11:47:57 +0900

    add local module

---
+++ src/Module/EnModule.php
@@ -0,0 +1,19 @@
+<?php
+namespace MyVendor\Locale\Module;
+
+use BEAR\Accept\AcceptModule;
+use BEAR\Package\PackageModule;
+use BEAR\Sunday\Module\Constant\NamedModule;
+use Ray\Di\AbstractModule;
+
+class EnModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $appDir = dirname(dirname(__DIR__));
+        $this->install(new NamedModule(require $appDir . '/var/locale/en.php'));
+    }
+}
