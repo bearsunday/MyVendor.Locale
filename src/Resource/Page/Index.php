6a93884d23afdb8ea6120eabdeff07a05b23dc3c
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-11 11:48:31 +0900

    inject locale message

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -2,12 +2,26 @@
 namespace MyVendor\Locale\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
+use Ray\Di\Di\Named;
 
 class Index extends ResourceObject
 {
+    /**
+     * @var string
+     */
+    private $greeting;
+
+    /**
+     * @Named("greeting")
+     */
+    public function __construct(string $greeting)
+    {
+        $this->greeting = $greeting;
+    }
+
     public function onGet(string $name = 'BEAR.Sunday') : ResourceObject
     {
-        $this['greeting'] = 'Hello ' . $name;
+        $this['greeting'] = $this->greeting . ' ' . $name;
 
         return $this;
     }
