ad35bcb2f25a0bba204a5cc68d5403d9bda4abf8
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-11 11:48:12 +0900

    content negotiation

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
 
-$context = 'prod-app';
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$available = [
+    'Accept' => [
+        'text/hal+json' => 'hal-app',
+        'application/json' => 'app',
+        'cli' => 'cli-hal-app'
+    ],
+    'Accept-Language' => [
+        'ja' => 'ja',
+        'en-US' => 'en'
+    ]
+];
+$accept = new \BEAR\Accept\Accept($available);
+list($context, $vary) = $accept($_SERVER);
+
 require dirname(__DIR__) . '/bootstrap/bootstrap.php';
